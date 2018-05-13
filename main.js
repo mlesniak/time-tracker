245eb108cd85699b9ea8cc35faa3a7878c5913d3
Michael Lesniak <mlesniak@micromata.de>
2018-05-13 13:58:26 +0200

    Move JavaScript to own file

---
+++ main.js
@@ -0,0 +1,35 @@
+var data = {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    datasets: [{
+        backgroundColor: "#00AA00",
+        borderColor: "#eeeeee",
+        borderWidth: 1,
+        data: [
+            200,
+            120,
+            80,
+            120,
+            10,
+            15,
+            60
+        ]
+    }]
+};
+
+window.onload = function() {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    window.chart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: {
+            responsive: true,
+            title: {
+                display: false,
+                text: 'Your last week'
+            },
+            legend: {
+                display: false,
+            },
+        }
+    });
+};
