4143a681d698082ab46561ace57d0aaee41bf611
Michael Lesniak <mlesniak@micromata.de>
2018-05-13 14:55:56 +0200

    Parse request body

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-// Install with npm install express --save
+// npm install --save express
+// npm install --save body-parser
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+app.use( bodyParser.json() );  
+app.use(express.static('public'));
 
 app.get('/api', (request, response) => {
     response.send('Hello from Express!')
 });
 
-app.use(express.static('public'));
+// Post new data. 
+app.post('/api', (request, response) => {
+    console.log(request.body);
+    response.send("OK");
+});
 
 app.listen(port, (err) => {
     if (err) {
