902c202f596667bb4eb61af4a0a8a2961b1d0f9a
Michael Lesniak <mlesniak@micromata.de>
2018-05-13 14:40:19 +0200

    Use express to server static files

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (request, response) => {
-  response.send('Hello from Express!')
-})
+app.get('/api', (request, response) => {
+    response.send('Hello from Express!')
+});
+
+app.use(express.static('public'));
 
 app.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-  console.log(`server is listening on ${port}`)
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
 });
